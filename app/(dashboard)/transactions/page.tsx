fcbe0e01f8da1950400b4eef19cdc6b2db4a619c
aaronjsharp <aaron.sharp2011@gmail.com>
2024-09-04 08:23:50 -0500

    Complete csv upload functionality

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -9,53 +9,96 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { useNewTransaction } from '@/features/transactions/hooks/use-new-transaction';
 import { useGetTransactions } from '@/features/transactions/api/use-get-transactions';
 import { useBulkDeleteTransactions } from '@/features/transactions/api/use-bulk-delete-transactions';
+import { useState } from 'react';
+import { UploadButton } from './upload-button';
+import { ImportCard } from './import-card';
 
+enum VARIANTS {
+  LIST = 'LIST',
+  IMPORT = 'IMPORT',
+}
 
+const INITIAL_IMPORT_RESULTS = {
+  data: [],
+  errors: [],
+  meta: {},
+};
 
 const TransactionsPage = () => {
-  const transactionsQuery = useGetTransactions()
-  const transactions = transactionsQuery.data || []
+  const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
+  const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS)
+
+  const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
+    console.log({ results })
+    setImportResults(results)
+    setVariant(VARIANTS.IMPORT)
+  }
+
+  const onCancelImport = () => {
+    setImportResults(INITIAL_IMPORT_RESULTS)
+    setVariant(VARIANTS.LIST)
+  }
+
+  const transactionsQuery = useGetTransactions();
+  const transactions = transactionsQuery.data || [];
   const newTransaction = useNewTransaction();
-  const deleteTransactions = useBulkDeleteTransactions()
+  const deleteTransactions = useBulkDeleteTransactions();
 
   const isDisabled =
-    transactionsQuery.isLoading ||
-    deleteTransactions.isPending
+    transactionsQuery.isLoading || deleteTransactions.isPending;
 
   if (transactionsQuery.isLoading) {
     return (
       <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
         <Card className='border-none drop-shadow-sm'>
-        <CardHeader>
-          <Skeleton className='h-8 w-48' />
-        </CardHeader>
-        <CardContent>
-          <div className='h-[500px] w-full flex items-center justify-center'>
-            <Loader2 className='size-6 text-slate-300 animate-spin'/>
-          </div>
-        </CardContent>
+          <CardHeader>
+            <Skeleton className='h-8 w-48' />
+          </CardHeader>
+          <CardContent>
+            <div className='h-[500px] w-full flex items-center justify-center'>
+              <Loader2 className='size-6 text-slate-300 animate-spin' />
+            </div>
+          </CardContent>
         </Card>
       </div>
-    )
+    );
   }
+
+  if (variant === VARIANTS.IMPORT) {
+    return (
+      <>
+        <ImportCard
+          data={importResults.data}
+          onCancel={onCancelImport}
+          onSubmit={() => {}}
+        />
+      </>
+    );
+  }
+
   return (
     <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
       <Card className='border-none drop-shadow-sm'>
         <CardHeader className='gap-y-2 lg:flex-row lg:items-center lg:justify-between'>
-          <CardTitle className='text-xl line-clamp-1'>Transactions History</CardTitle>
-          <Button size='sm' onClick={newTransaction.onOpen}>
-            <Plus className='size-4 mr-2' />
-            Add new
-          </Button>
+          <CardTitle className='text-xl line-clamp-1'>
+            Transactions History
+          </CardTitle>
+          <div className='flex items-center gap-x-2'>
+            <Button size='sm' onClick={newTransaction.onOpen}>
+              <Plus className='size-4 mr-2' />
+              Add new
+            </Button>
+            <UploadButton onUpload={onUpload} />
+          </div>
         </CardHeader>
         <CardContent>
-          <DataTable 
-            filterKey='payee' 
-            columns={columns} 
-            data={transactions} 
+          <DataTable
+            filterKey='payee'
+            columns={columns}
+            data={transactions}
             onDelete={(row) => {
-              const ids = row.map((r) => r.original.id)
-              deleteTransactions.mutate({ ids })
+              const ids = row.map((r) => r.original.id);
+              deleteTransactions.mutate({ ids });
             }}
             disabled={isDisabled}
           />
